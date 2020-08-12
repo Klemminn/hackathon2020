70ed824ff341c889a083850293496451411d043a
klemminn <klemenz88@gmail.com>
2020-08-12 18:52:11 +0000

    Add basic components and dependencies

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export * from './buttons'
+export * from './inputs'
+export * from './layout'
+
+export { default as Header } from './Header'
+export { default as Link } from './Link'
+export { default as Spinner } from './Spinner'
