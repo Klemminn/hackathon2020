cb7b6767bb88522d5206b0f8b6e5a9502304d349
klemminn <klemenz88@gmail.com>
2020-08-19 00:09:47 +0000

    Move participant titles from frontend to backend

--- src/services/ParticipantService.tsx
+++ src/services/ParticipantService.tsx
@@ -1,49 +1,8 @@
 import connector from "./connector";
 
-const titles = [
-  {
-    points: 0,
-    title: "Nýgræðlingur"
-  },
- 
-  {
-    points: 5,
-    title: "Græðlingur"
-  },
-  {
-    points: 10,
-    title: "Viðarvinur"
-  },
-  {
-    points: 20,
-    title: "Trjáaldur"
-  },
-  {
-    points: 30,
-    title: "Trjáhvíslari"
-  }
-  ,
-  {
-    points: 40,
-    title: "Skóghirðir"
-  }
-  ,
-  {
-    points: 50,
-    title: "Entur"
-  },
-  {
-    points: 100,
-    title: "Landvættur"
-  }
-];
-
 class ParticipantService {
   static async getParticipant(body: { email: string; name: string }) {
     const { data } = await connector.post("/participant/", body);
-
-    data.title = this.getTitle(data.co2Offset);
-
     return data;
   }
 
@@ -55,12 +14,7 @@ class ParticipantService {
 
   static async getLeaderboard() {
     const { data } = await connector.get("/leaderboard/");
-    data.map( (x: any) =>  x.title = this.getTitle(x.totalCo2)) ;  
     return data;
-  }
-
-  static getTitle(points : number){
-    return ( titles.find(x=> x.points >= points) || titles[titles.length-1]).title;
   }
 }
 
--- src/states/ParticipantState.tsx
+++ src/states/ParticipantState.tsx
@@ -6,17 +6,19 @@ import { Participant } from 'types'
 const defaultParticipant = {
   name: '',
   email: '',
-  co2Offset: 0
+  co2Offset: 0,
+  title: ''
 }
 
 export const ParticipantState = createStateLink(defaultParticipant)
 
 export const setParticipant = (participant?: Participant) => {
+  console.log(participant)
   ParticipantState.set(participant || defaultParticipant)
 }
 
 export const updateCo2 = async () => {
   const current = ParticipantState.get()
-  const { co2Offset } = await ParticipantService.getParticipant(current)
-  ParticipantState.set({ ...current, co2Offset })
+  const { co2Offset, title } = await ParticipantService.getParticipant(current)
+  ParticipantState.set({ ...current, co2Offset, title })
 }
